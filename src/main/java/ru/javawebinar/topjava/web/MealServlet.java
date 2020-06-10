3fa7ef7f20daa9e9d409dd78b1fe7e210aca8bd8
SIMMA <60348525+Simma27@users.noreply.github.com>
2020-06-10 16:35:11 +0300

    Need to correct

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -8,7 +8,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -23,7 +22,8 @@ import static org.slf4j.LoggerFactory.getLogger;
 public class MealServlet extends HttpServlet {
 
     private static final Logger log = getLogger(MealServlet.class);
-    List<Meal> meals = Arrays.asList(
+
+    private final List<Meal> meals = Arrays.asList(
             new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
             new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000),
             new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500),
@@ -34,19 +34,22 @@ public class MealServlet extends HttpServlet {
     );
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        log.debug("redirect to users");
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.debug("redirect to meal");
 
-        List<MealTo> mealTos = filtredByMeal(meals,2000);
-        request.getRequestDispatcher("/meals.jsp").forward(request, response);
+        final List<MealTo> mealsTo = filteredByMeal(meals, 2000);
+        req.setAttribute("meals", mealsTo);
+
+        req.getRequestDispatcher("meals.jsp").forward(req, resp);
     }
 
-    private List<MealTo> filtredByMeal(List<Meal> meals, int caloriesPerDay){
+    private List<MealTo> filteredByMeal(List<Meal> meals, int caloriesPerDay) {
         Map<LocalDate, Integer> caloriesSumByDate = meals.stream()
-                .collect(
-                        Collectors.groupingBy(Meal::getDate, Collectors.summingInt(Meal::getCalories)));
-        return meals.stream().map(meal ->
-            new MealTo(meal.getDateTime(), meal.getDescription(), meal.getCalories(), caloriesSumByDate.get(meal.getDate()) > caloriesPerDay))
+                .collect(Collectors.groupingBy(Meal::getDate, Collectors.summingInt(Meal::getCalories)));
+
+        return meals.stream()
+                .map(meal -> new MealTo(meal.getDateTime(), meal.getDescription(), meal.getCalories(), caloriesSumByDate.get(meal.getDate()) > caloriesPerDay))
                 .collect(Collectors.toList());
     }
+
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,3 +1,4 @@
+
 <%@ page contentType="text/html;charset=UTF-8" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
@@ -8,7 +9,7 @@
 <body>
 <h3><a href="index.html">Home</a></h3>
 <hr>
-<table border="5" align="center">
+<table border="5" align="left">
     <thead>
     <tr>
         <th>Date</th>
@@ -19,11 +20,11 @@
     <tbody>
     <c:forEach items="${meals}" var="meal">
         <tr style="background-color:${meal.isExcess() ? 'red' : 'green'}">
-            <td><b><fmt:parseDate value="${meal.getDateTime()}" pattern="yyyy-MM-dd'T'HH:mm"
+            <td><fmt:parseDate value="${meal.getDateTime()}" pattern="yyyy-MM-dd'T'HH:mm"
                                   var="parsedDateTime" type="both"/>
-                <fmt:formatDate pattern="dd.MM.yyyy HH:mm" value="${parsedDateTime}"/></b></td>
-            <td><b><c:out value="${meal.getDescription()}"/></b></td>
-            <td><b><c:out value="${meal.getCalories()}"/></b></td>
+                <fmt:formatDate pattern="dd.MM.yyyy HH:mm" value="${parsedDateTime}"/></td>
+            <td><c:out value="${meal.getDescription()}"/></td>
+            <td><c:out value="${meal.getCalories()}"/></td>
         </tr>
     </c:forEach>
     </tbody>
