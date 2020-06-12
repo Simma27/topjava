d5bf9dd51d09c2727d355d1ed0586f5b4d5fb250
SIMMA <60348525+Simma27@users.noreply.github.com>
2020-06-12 15:01:25 +0300

    HomeWork 2.2

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -5,16 +5,28 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class Meal {
+
+    private final Integer id;
+
     private final LocalDateTime dateTime;
 
     private final String description;
 
     private final int calories;
 
-    public Meal(LocalDateTime dateTime, String description, int calories) {
+    public Meal(Integer id, LocalDateTime dateTime, String description, int calories) {
+        this.id = id;
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Integer setId() {
+        this.id = id;
     }
 
     public LocalDateTime getDateTime() {
@@ -35,5 +47,9 @@ public class Meal {
 
     public LocalTime getTime() {
         return dateTime.toLocalTime();
+    }
+
+    public void isNew(Integer id){
+
     }
 }
